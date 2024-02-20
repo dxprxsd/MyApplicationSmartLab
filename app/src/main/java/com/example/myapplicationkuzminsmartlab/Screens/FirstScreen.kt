5ca1336ce87ae7885906486abc277c2987473ec5
Ivan Kuzmin <ivan26drag@gmail.com>
2024-02-20 19:08:27 +0300

    first screen new

--- app/src/main/java/com/example/myapplicationkuzminsmartlab/Screens/FirstScreen.kt
+++ app/src/main/java/com/example/myapplicationkuzminsmartlab/Screens/FirstScreen.kt
@@ -7,10 +7,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
